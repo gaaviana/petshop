3db212042f02998150566671c338d35ad093f88e
Gabriel Viana <gaa.vianaa@gmail.com>
2025-07-30 09:41:00 -0300

    fonts e metadados

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,37 @@
 import type { Metadata } from "next";
+import {Roboto, Fjalla_One} from "next/font/google"
 import "./globals.css";
+
+/* Configuea variáveis de fontes */
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-roboto',
+  display: 'swap'
+})
+
+const fjallaOne = Fjalla_One({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-fjalla',
+  display: 'swap'
+})
 
 export const metadata: Metadata = {
   title: "PetShop",
   description: "Mini portal de Petshop com notícias, produtos e muito mais",
+  keywords: [
+    'petshop','banho e tosa','ração para cães','ração para gatos','produtos pet','vacinação pet','veterinário','brinquedos para pets','caminha para cachorro','coleiras e guias','petshop online','hotel para cães','acessórios para pets','antipulgas','comida natural para pets','petiscos saudáveis','creche para pets','petshop perto de mim','consulta veterinária','loja de animais','cuidados com filhotes','produtos para gatos','atendimento pet domiciliar'
+  ],
+  authors: [
+    {
+      name: 'Gabriel Viana Paulino',
+      url: 'https://github.com/gaaviana'
+    }
+  ]
 };
+
 
 export default function RootLayout({
   children,
@@ -13,7 +40,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body>
+      <body className={`${roboto.variable} ${fjallaOne.variable}`}>
         <header>
           <h1>PetShop</h1>
         </header>
