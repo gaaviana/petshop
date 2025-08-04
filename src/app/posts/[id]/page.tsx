62fd912d4b5b6900efaa1ac02571a4ed45a4a337
Gabriel Viana <gaa.vianaa@gmail.com>
2025-08-04 11:56:15 -0300

    exercicio

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,18 +1,47 @@
 import Container from "@/components/Container";
-import estilos from './detalhe-post.module.css'
+import estilos from "./detalhe-post.module.css";
+import { Post } from "@/types/Post";
+import { title } from "process";
 
 type DetalhePostProps = {
-    params: Promise<{id: string}>
+  params: Promise<{ id: string }>;
+};
+
+async function buscarPostPorId({ params }: DetalhePostProps): Promise<Post> {
+  const { id } = await params;
+
+  const res = await fetch(`http://localhost:2112/posts/${id}`, {
+    next: { revalidate: 0 },
+  });
+
+  if (!res.ok) {
+    throw new Error("Erro ao buscar os posts: " + res.statusText);
+  }
+
+  const post: Post = await res.json();
+
+  return post;
 }
 
-export default function DetalhePost({params}:DetalhePostProps) {
+export async function generateMetadata({ params }: DetalhePostProps) {
+  const post = await buscarPostPorId({ params });
+
+  return {
+    title: post.titulo + " | Petshop",
+    description: post.descricao,
+  };
+}
+
+export default async function DetalhePost({ params }: DetalhePostProps) {
+  const post = await buscarPostPorId({ params });
+
   return (
-   <article className={estilos.conteudo}>
-    <h2></h2>
-    <Container>
-        <h3></h3>
-        <p></p>
-    </Container>
-   </article>
-  )
+    <article className={estilos.conteudo}>
+      <h2>{post.titulo}</h2>
+      <Container>
+        <h3>{post.categoria}</h3>
+        <p>{post.descricao}</p>
+      </Container>
+    </article>
+  );
 }
