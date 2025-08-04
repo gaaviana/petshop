f8d1a1ce0fcf17b3091671b8b9b6e26298def05f
Gabriel Viana <gaa.vianaa@gmail.com>
2025-08-04 09:06:24 -0300

    comunicação com o db.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,19 @@
 import ListaPosts from "@/components/ListaPosts";
 import estilos from "./page.module.css";
 import arrayPosts from "@/data/array-posts";
+import { log } from "console";
 
-export default function Home() {
+export default async function Home() {
+
+  const res = await fetch('http://localhost:2112/posts');
+
+  if(!res.ok){
+    throw new Error('Erro ao buscar os posts: '+res.statusText)
+  }
+
+  const posts = await res.json()
+  log(posts)
+
   return (
     <section className={estilos.conteudo}>
       <h2>Pet Notícias</h2>
