6f0789ba589513f77f20c7e6cd784ca3b534c872
Gabriel Viana <gaa.vianaa@gmail.com>
2025-08-04 09:50:01 -0300

    preparando a pag

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,14 @@
+import Container from "@/components/Container";
+import estilos from './detalhe-post.module.css'
+
+export default function DetalhePost() {
+  return (
+   <article className={estilos.conteudo}>
+    <h2></h2>
+    <Container>
+        <h3></h3>
+        <p></p>
+    </Container>
+   </article>
+  )
+}
