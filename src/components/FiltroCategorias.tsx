314aebacc03532f60c80e91d557221f2aaa14a38
Gabriel Viana <gaa.vianaa@gmail.com>
2025-08-18 08:49:39 -0300

    cria componente com estilos

---
+++ src/components/FiltroCategorias.tsx
@@ -0,0 +1,11 @@
+import estilos from './FiltroCategorias.module.css'
+
+export default function FiltroCategorias() {
+  return (
+    <div className={estilos.categorias}>
+      <button>Categoria A</button>
+      <button>Categoria B</button>
+      <button className={estilos.limpar}>Limpar</button>
+    </div>
+  )
+}
